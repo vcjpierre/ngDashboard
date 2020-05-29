dae1e3e7354373f2abf9d0d50cb734610fdae09c
Jean Pierre Velez <vcjpierre@gmail.com>
2020-05-29 11:39:35 -0400

    Add models

---
+++ api/models/Customer.cs
@@ -0,0 +1,10 @@
+namespace api.Models
+{
+  public class Customer
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string State { get; set; }
+  }
+}
---
+++ api/models/Server.cs
@@ -0,0 +1,9 @@
+namespace api.Models
+{
+  public class Server
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsOnline { get; set; }
+  }
+}

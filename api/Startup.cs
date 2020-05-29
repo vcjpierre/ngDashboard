3d65d636f7877311e3ec6d7722ef022ec3824051
Jean Pierre Velez <vcjpierre@gmail.com>
2020-05-29 11:39:55 -0400

    Config cors

--- api/Startup.cs
+++ api/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,18 +27,41 @@ namespace api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(opt => 
+                {
+                    opt.AddPolicy("CorsPolicy",
+                        b => b.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials()
+                    );
+                }
+            );
+
             services.AddControllers();
+
+            services.AddEntityFrameworkNpgsql()
+                .AddDbContext<ApiContext>(
+                    opt => opt.UseNpgsql(Configuration.GetConnectionString("AdvantageApi"))
+                );
+
+            services.AddTransient<DataSeed>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataSeed seeder)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCors("CorsPolicy");
             }
 
-            app.UseHttpsRedirection();
+            var nCustomers = 20;
+            var nOrders = 1000;
+            seeder.SeedData(nCustomers, nOrders);
+
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
@@ -44,7 +69,7 @@ namespace api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute("default", "api/{controller}/{action}/{id?}");
             });
         }
     }
